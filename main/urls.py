b8469da8b79b4caa7c20cccb0e366dd4b2a26e12
Ivy Kabere <kabereivy@gmail.com>
2022-06-18 00:21:53 +0300

    add url for signup form

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+     path('signup/', views.signup, name='signup'),
      path('', views.index, name='index'),
      path('hood/', views.hood, name='hood'),
 ]
