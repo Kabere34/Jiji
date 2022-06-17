4091f70bc697f07c33a0e9fbe0aaa576dd0cf234
Ivy Kabere <kabereivy@gmail.com>
2022-06-18 00:21:32 +0300

    add Signup form

---
+++ main/forms.py
@@ -0,0 +1,12 @@
+from models import *
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+  email=forms.EmailField(max_length=250, help_text='Required. Inform a valid email address.')
+
+  class Meta:
+    model=User
+    fields=('username','email','password1','password2')
+
+
