6ac40f9d8dff7f6b02e836921a07670e52a716c7
Ivy Kabere <kabereivy@gmail.com>
2022-06-18 00:07:02 +0300

    add function to fetch all hoods

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
      path('', views.index, name='index'),
+     path('hood/', views.hood, name='hood'),
 ]
 
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-  return render(request,'main/index.html')
+  current_user=request.user
+  posts=Post.objects.all()
+  context={
+    "posts":posts
+  }
+  return render(request,'main/index.html',context)
+
+
+def hood(request):
+  current_user=request.user
+  hoods=Neighbourhood.objects.all()
+  context={
+    "hoods":hoods
+  }
+  return render(request,'main/hood.html',context)
