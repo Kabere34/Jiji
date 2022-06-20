8867756f0e6e9daff0c8dfb4f8effc8c75c8e7fd
Ivy Kabere <kabereivy@gmail.com>
2022-06-20 14:50:27 +0300

    edit Neighbourhood model

--- main/models.py
+++ main/models.py
@@ -8,6 +8,8 @@ class Neighbourhood(models.Model):
   name = models.CharField(max_length=50)
   location=models.CharField(max_length=50,default="e.g Nairobi, Juja, Kiambu etc")
   occupants=models.IntegerField(default=0,blank=True)
+  admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+  hood_img=models.ImageField(upload_to='hood_images/')
   description = models.TextField()
   health_tell = models.IntegerField(null=True, blank=True)
   police_number = models.IntegerField(null=True, blank=True)
@@ -15,13 +17,21 @@ class Neighbourhood(models.Model):
   def __str__(self):
     return f'{self.name} jiji'
 
+  def create_neighborhood(self):
+        self.save()
+
+  def delete_neighborhood(self):
+        self.delete()
+
+
+
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
   name= models.CharField(max_length=50)
   bio= models.CharField(max_length=250, blank=True,default="My Bio")
   avatar=models.ImageField(upload_to='avatars/', default='default.png')
   location=models.CharField(max_length=50, blank=True, null=True)
-  neighbourhood=models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name='members', blank=True)
+  neighbourhood=models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL,null=True,related_name='members', blank=True)
 
   def __str__(self):
     return f'{self.user.username} profile'
@@ -37,10 +47,15 @@ class Business(models.Model):
   def __str__(self):
         return f'{self.name} Business'
 
+  @classmethod
+  def search_by_name(cls, search_term):
+    business=cls.objects.filter(name__icontains=search_term)
+    return business
+
 class Post(models.Model):
   title=models.CharField(max_length=120, null=True)
   post=models.TextField()
   date=models.DateTimeField(auto_now_add=True)
   user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-  image=models.ImageField(upload_to='images')
+  image=models.ImageField(upload_to='images',null=True)
   hood=models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
